dad31fb3f53d5bddbf466dc6fd2fb3b39f3bb9fc
rothsteins <rothsteins@LR064W78-1718.ellis.int>
2019-04-02 18:40:47 -0400

    I'm sorry I can't make a meme so I just wrote some random text

--- src/ImagePanel.java
+++ src/ImagePanel.java
@@ -1,4 +1,5 @@
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -36,7 +37,14 @@ public class ImagePanel extends JPanel {
 	}
 	
 	public void paintComponent(Graphics g) { 
+		g.setFont(new Font ("TimesRoman", Font.BOLD, 15));
 		g.drawImage(image, 0, 0, null); 
+		g.drawString(" what should I say....? ", 35, 25);
+	}
+	
+	public Dimension getPreferredSize() { 
+		Dimension size = new Dimension(width, height); 
+		return size; 
 	}
 
 }
